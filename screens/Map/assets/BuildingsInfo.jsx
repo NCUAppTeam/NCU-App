5cb3f24579b72c13452893fb858922dc7a01bf42
tonyYZL <t0965176327@gmail.com>
2022-06-23 19:55:27 +0800

    add location info

--- screens/Map/assets/BuildingsInfo.jsx
+++ screens/Map/assets/BuildingsInfo.jsx
@@ -4,6 +4,7 @@ export default [
     longitude: 121.19367,
     type: '系館',
     name: 'I 志希館',
+    opentime: ['平日｜08:00-22:00', '六日｜不開放', '寒暑假平日｜08:00-17:00'],
     units: '電子計算機中心, 管理學院研究室, 管理學院辦公室',
   },
   {
@@ -11,6 +12,7 @@ export default [
     longitude: 121.19078,
     type: '校內店家',
     name: '松苑餐廳',
+    opentime: ['每日｜07:30-21:30', '寒暑假｜另行公告'],
     units: '漢堡王, 全家, 路易莎, lala kitchen, 都挺好',
   },
   {
@@ -18,13 +20,15 @@ export default [
     longitude: 121.19465,
     type: '宿舍',
     name: '國際學生宿舍',
+    opentime: ['平日｜'],
     units: ' ',
   },
   {
     latitude: 24.96967,
     longitude: 121.19069,
     type: '體育場所',
-    name: '室外籃/排球場',
+    name: '室外籃球場',
+    opentime: ['每天｜07:00-22:00'],
     units: ' ',
   },
   {
@@ -32,6 +36,7 @@ export default [
     longitude: 121.19431,
     type: '宿舍',
     name: '女十四舍',
+    opentime: ['平日｜'],
     units: '藝文空間, 拉亞漢堡, 龍騰影印店, 581生活館, 全家無人商店',
   },
   {
@@ -39,6 +44,7 @@ export default [
     longitude: 121.19511,
     type: '宿舍',
     name: '男十一舍',
+    opentime: ['平日｜'],
     units: '敦煌書局, 中大眼鏡行, 印象剪髮, 郵局, 洗衣店',
   },
   {
@@ -46,6 +52,7 @@ export default [
     longitude: 121.19519,
     type: '系館',
     name: 'LS 人文社會科學大樓',
+    opentime: ['平日｜'],
     units: '文三, 文學三館, 黑盒子劇場, 藝文中心, 107電影院 文學院辦公室',
   },
   {
@@ -53,6 +60,7 @@ export default [
     longitude: 121.195814,
     type: '宿舍',
     name: '校長宿舍',
+    opentime: ['平日｜'],
     units: ' ',
   },
   {
@@ -60,6 +68,7 @@ export default [
     longitude: 121.19292,
     type: '系館',
     name: 'O 綜教館',
+    opentime: ['平日｜'],
     units: '語言中心',
   },
   {
@@ -67,14 +76,15 @@ export default [
     longitude: 121.19368,
     type: '行政處室',
     name: 'L 中正圖書館',
-    units:
-      '舊圖, K書中心, 生活輔導組, 課外活動組, 服務學習辦公室, 軍訓室, 衛生保健組, 圖書館視聽室, 原住民族學生資源中心, 諮商中心, 資源教室, 藝文中心展示空間, 職涯發展中心, 小中大電視台, 備用書庫, 英語自學中心, 戲曲研究室, 團體活動室, 學務處',
+    opentime: ['平日｜'],
+    units: '舊圖, K書中心, 生活輔導組, 課外活動組, 服務學習辦公室, 軍訓室, 衛生保健組, 圖書館視聽室, 原住民族學生資源中心, 諮商中心, 資源教室, 藝文中心展示空間, 職涯發展中心, 小中大電視台, 備用書庫, 英語自學中心, 戲曲研究室, 團體活動室, 學務處',
   },
   {
     latitude: 24.966554536362217,
     longitude: 121.18774915643395,
     type: '系館',
     name: '前瞻科技中心',
+    opentime: ['平日｜'],
     units: ' ',
   },
   {
@@ -82,6 +92,7 @@ export default [
     longitude: 121.1935,
     type: '校內店家',
     name: '小木屋鬆餅',
+    opentime: ['平日｜08:00-19:30', '假日|09:30-18:30'],
     units: ' ',
   },
   {
@@ -89,6 +100,7 @@ export default [
     longitude: 121.19555,
     type: '宿舍',
     name: '男七舍',
+    opentime: ['平日｜'],
     units: ' ',
   },
   {
@@ -96,6 +108,7 @@ export default [
     longitude: 121.19097,
     type: '體育場所',
     name: '溜冰場',
+    opentime: ['平日｜'],
     units: 'skating',
   },
   {
@@ -103,6 +116,7 @@ export default [
     longitude: 121.19446,
     type: '行政處室',
     name: '住宿服務處',
+    opentime: ['平日｜'],
     units: ' ',
   },
   {
@@ -110,6 +124,7 @@ export default [
     longitude: 121.19091,
     type: '體育場所',
     name: 'YH 依仁堂',
+    opentime: ['平日｜17:00-21:00', '六日｜不開放', '寒暑假｜08:00-17:00'],
     units: '室內籃球場, 室內排球場, 健身房, 韻律教室, 桌球練習場, 技擊教室',
   },
   {
@@ -117,6 +132,7 @@ export default [
     longitude: 121.19381,
     type: '社團活動區',
     name: '志道樓',
+    opentime: ['平日｜'],
     units: ' ',
   },
   {
@@ -124,6 +140,7 @@ export default [
     longitude: 121.19448436732834,
     type: '社團活動區',
     name: 'L3 國鼎圖書資料館',
+    opentime: ['平日｜'],
     units: '國鼎圖書館, 白樓特藏室, 國鼎館展示室, 簡報室, 閱覽室',
   },
   {
@@ -131,14 +148,15 @@ export default [
     longitude: 121.19209,
     type: '系館',
     name: 'S4 健雄館',
-    units:
-      '科四館, 科學四館, 物理系, 奈米科技研究中心, 探測器研發中心, 複雜系統研究中心, 中子束應用研究中心, 數學與理論物理中心, 中壢福衛地面站, 天文研究所, 太空科學研究所',
+    opentime: ['平日｜'],
+    units: '科四館, 科學四館, 物理系, 奈米科技研究中心, 探測器研發中心, 複雜系統研究中心, 中子束應用研究中心, 數學與理論物理中心, 中壢福衛地面站, 天文研究所, 太空科學研究所',
   },
   {
     latitude: 24.96718,
     longitude: 121.19571,
     type: '景點',
     name: 'AH 大講堂',
+    opentime: ['平日｜'],
     units: '校史館',
   },
   {
@@ -146,6 +164,7 @@ export default [
     longitude: 121.19435,
     type: '宿舍',
     name: '研究生宿舍',
+    opentime: ['平日｜'],
     units: '曦望居',
   },
   {
@@ -153,38 +172,39 @@ export default [
     longitude: 121.19269,
     type: '系館',
     name: 'E 工程一館',
-    units:
-      '土木工程學系, 化學工程與材料工程學系, 化材, 奈米觸媒研究中心, 界面科技研究中心 , 土木材料品保中心, 災害防治研究中心, 橋梁工程研究中心',
+    opentime: ['平日｜'],
+    units: '土木工程學系, 化學工程與材料工程學系, 化材, 奈米觸媒研究中心, 界面科技研究中心 , 土木材料品保中心, 災害防治研究中心, 橋梁工程研究中心',
   },
   {
     latitude: 24.96946,
     longitude: 121.19458,
     type: '系館',
     name: 'A 文學一館',
-    units:
-      '中國文學系, 英美語文學系, 法國語文學系, 哲學研究所, 歷史研究所, 藝術學研究所, 學習與教學研究及亞祭文化研究國際碩士學位學程所',
+    opentime: ['平日｜'],
+    units: '中國文學系, 英美語文學系, 法國語文學系, 哲學研究所, 歷史研究所, 藝術學研究所, 學習與教學研究及亞祭文化研究國際碩士學位學程所',
   },
   {
     latitude: 24.968898292544104,
     longitude: 121.18847797691994,
     type: '系館',
     name: 'E5 大型力學實驗室',
-    units:
-      '結構力學實驗室, 韓鋼架及混凝土力學實驗區, 大地工程實驗室, 流體力學實驗室',
+    opentime: ['平日｜'],
+    units: '結構力學實驗室, 韓鋼架及混凝土力學實驗區, 大地工程實驗室, 流體力學實驗室',
   },
   {
     latitude: 24.9672,
     longitude: 121.19465,
     type: '系館',
     name: 'S 科學一館',
-    units:
-      '地球科學學院辦公室, 地球科學系, 地球物理研究所, 水文與海洋科學研究所, 地震電磁前兆研究中心, 應用地質研究所, 環境研究中心, 中大天文台, 秉文堂',
+    opentime: ['平日｜'],
+    units: '地球科學學院辦公室, 地球科學系, 地球物理研究所, 水文與海洋科學研究所, 地震電磁前兆研究中心, 應用地質研究所, 環境研究中心, 中大天文台, 秉文堂',
   },
   {
     latitude: 24.96604511545328,
     longitude: 121.19190498514823,
     type: '系館',
     name: '中大幼稚園',
+    opentime: ['平日｜'],
     units: ' ',
   },
   {
@@ -192,6 +212,7 @@ export default [
     longitude: 121.19569,
     type: '校內店家',
     name: '中央大學第九宿舍餐廳',
+    opentime: ['每日｜11:00-20:00'],
     units: '全家, 四海遊龍, 金盃美而美早餐, 賈董的麵, 香米自助餐, 第九餐廳',
   },
   {
@@ -199,6 +220,7 @@ export default [
     longitude: 121.19343,
     type: '宿舍',
     name: '男三舍',
+    opentime: ['平日｜'],
     units: ' ',
   },
   {
@@ -206,6 +228,7 @@ export default [
     longitude: 121.18941,
     type: '體育場所',
     name: '攀岩塔',
+    opentime: ['平日＆寒暑假｜08:00-17:00 (上課及申請使用)', '六日｜08:00-17:00 (付費使用)'],
     units: '攀岩場',
   },
   {
@@ -213,6 +236,7 @@ export default [
     longitude: 121.19548,
     type: '宿舍',
     name: '男九舍',
+    opentime: ['平日｜'],
     units: '',
   },
   {
@@ -220,6 +244,7 @@ export default [
     longitude: 121.19583,
     type: '社團活動區',
     name: '據德樓',
+    opentime: ['平日｜'],
     units: '社團辦公室',
   },
   {
@@ -227,6 +252,7 @@ export default [
     longitude: 121.19495,
     name: 'HO2 中大會館',
     type: '景點',
+    opentime: ['平日｜'],
     units: ' ',
   },
   {
@@ -234,6 +260,7 @@ export default [
     longitude: 121.1958,
     type: '宿舍',
     name: '男六舍',
+    opentime: ['平日｜'],
     units: ' ',
   },
   {
@@ -241,6 +268,7 @@ export default [
     longitude: 121.193,
     type: '體育場所',
     name: '棒壘球場',
+    opentime: ['每天｜07:00-17:00'],
     units: '棒球場, 壘球場, 大草坪',
   },
   {
@@ -248,37 +276,39 @@ export default [
     longitude: 121.19087,
     type: '行政處室',
     name: '國際處&創新育成中心',
-    units: ' ',
+    opentime: ['平日｜'],
+    units: 'IC 松果館',
   },
   {
     latitude: 24.97102,
     longitude: 121.19037,
     type: '系館',
     name: 'HK 客家學院大樓',
-    units:
-      '客家與社會學系, 客家學院辦公室, 客家社會文化研究所, 客家語文研究所, 客家政治經濟研究所, 客家研究在職專班, 法律與政府研究所, 客語認證中心, 客家公共事務中心, 客家希望工程言青年培育中心, 全球客家女性研究(基金)中心, 客家藝文中心',
+    opentime: ['平日｜'],
+    units: '客家與社會學系, 客家學院辦公室, 客家社會文化研究所, 客家語文研究所, 客家政治經濟研究所, 客家研究在職專班, 法律與政府研究所, 客語認證中心, 客家公共事務中心, 客家希望工程言青年培育中心, 全球客家女性研究(基金)中心, 客家藝文中心',
   },
   {
     latitude: 24.96861,
     longitude: 121.18767,
     type: '系館',
     name: 'E3 環工化工館',
-    units:
-      '工程四館, 環境工程學研究所, 永續環境科技研究中心, 化學工程與材料工程學系實驗館',
+    opentime: ['平日｜'],
+    units: '工程四館, 環境工程學研究所, 永續環境科技研究中心, 化學工程與材料工程學系實驗館',
   },
   {
     latitude: 24.97162,
     longitude: 121.19269,
     type: '系館',
     name: 'S5 科學五館',
-    units:
-      '腦科學研究中心, 生物醫藥研發中心, 認知與神經科學所, 系統生物與生物資訊研究所, 生命科學系',
+    opentime: ['平日｜'],
+    units: '腦科學研究中心, 生物醫藥研發中心, 認知與神經科學所, 系統生物與生物資訊研究所, 生命科學系',
   },
   {
     latitude: 24.96839,
     longitude: 121.19443,
     type: '系館',
     name: 'L2 總圖書館',
+    opentime: ['平日｜'],
     units: '英語閱讀坊, 台聯大圖書管理系統',
   },
   {
@@ -286,6 +316,7 @@ export default [
     longitude: 121.194957,
     type: '宿舍',
     name: '男十二舍',
+    opentime: ['平日｜'],
     units: ' ',
   },
   {
@@ -293,6 +324,7 @@ export default [
     longitude: 121.1909,
     type: '體育場所',
     name: '羽球館',
+    opentime: ['平日｜07:00-12:00、13:00-22:00', '六日｜07:00-12:00、14:00-17:00', '寒暑假｜07:00-12:00、14:00-17:00'],
     units: ' ',
   },
   {
@@ -300,6 +332,7 @@ export default [
     longitude: 121.18709,
     type: '系館',
     name: 'R3 研究中心大樓二期',
+    opentime: ['平日｜'],
     units: '生醫理工學院, 生醫理工學院辦公室',
   },
   {
@@ -307,14 +340,15 @@ export default [
     longitude: 121.19522,
     type: '行政處室',
     name: 'A1 行政大樓',
-    units:
-      '教務處, 總務處, 註冊組, 課務組, 招生組, 文書組, 出納組, 保管組, 事務組',
+    opentime: ['平日｜'],
+    units: '教務處, 總務處, 註冊組, 課務組, 招生組, 文書組, 出納組, 保管組, 事務組',
   },
   {
     latitude: 24.96828,
     longitude: 121.19234,
     type: '系館',
     name: 'TR 教學研究綜合大樓暨大禮堂',
+    opentime: ['平日｜'],
     units: '世界奇觀, 大禮堂, 多功能會議廳, 教學研究空間, 停車場, 教研大樓',
   },
   {
@@ -322,30 +356,31 @@ export default [
     longitude: 121.19453,
     type: '系館',
     name: 'C2 文學二館',
-    units:
-      '中國文學系, 英美語文學系, 法國語文學系, 哲學研究所, 歷史研究所, 藝術學研究所, 學習與教學研究及亞祭文化研究國際碩士學位學程所',
+    opentime: ['平日｜'],
+    units: '中國文學系, 英美語文學系, 法國語文學系, 哲學研究所, 歷史研究所, 藝術學研究所, 學習與教學研究及亞祭文化研究國際碩士學位學程所',
   },
   {
     latitude: 24.97088,
     longitude: 121.19242,
     type: '系館',
     name: 'S2 科學三館',
-    units:
-      '物化實驗室, 有機無機實驗室, 核磁共振儀室, 普化分析實驗室, 大氣分析實驗室, 光子材料研究實驗室, 高分子及觸媒化學實驗室, 有機化學研究實驗室, 有機光電材料開發化學實驗室, 有機化學研究實驗室, 有機光電材料開發實驗室',
+    opentime: ['平日｜'],
+    units: '物化實驗室, 有機無機實驗室, 核磁共振儀室, 普化分析實驗室, 大氣分析實驗室, 光子材料研究實驗室, 高分子及觸媒化學實驗室, 有機化學研究實驗室, 有機光電材料開發化學實驗室, 有機化學研究實驗室, 有機光電材料開發實驗室',
   },
   {
     latitude: 24.9678,
     longitude: 121.18858,
     type: '系館',
     name: 'E2 機械館',
-    units:
-      '工程三館, 機械工程學系, 機械系, 能源研究中心, 生物醫學工程研究所, 能源工程研究所, 光機電工程研究所',
+    opentime: ['平日｜'],
+    units: '工程三館, 機械工程學系, 機械系, 能源研究中心, 生物醫學工程研究所, 能源工程研究所, 光機電工程研究所',
   },
   {
     latitude: 24.96788,
     longitude: 121.19153,
     type: '系館',
     name: 'E1 工程二館',
+    opentime: ['平日｜'],
     units:
       '電機工程學系, 通訊工程學系, 電機系, 通訊系, 小禮堂, 視聽教室, 通訊工程研究所, 資訊電機學院',
   },
@@ -354,6 +389,7 @@ export default [
     longitude: 121.1953,
     type: '宿舍',
     name: '男五舍',
+    opentime: ['平日｜'],
     units: ' ',
   },
   {
@@ -361,6 +397,7 @@ export default [
     longitude: 121.19584,
     type: '社團活動區',
     name: '游藝館',
+    opentime: ['平日｜'],
     units: '社團辦公室',
   },
   {
@@ -368,6 +405,7 @@ export default [
     longitude: 121.19542,
     type: '校內店家',
     name: '松果餐廳',
+    opentime: ['平日｜'],
     units: '全家, 吮指王炸雞, 豪秋吐司, 香米自助餐',
   },
   {
@@ -375,6 +413,7 @@ export default [
     longitude: 121.19048,
     type: '系館',
     name: 'IL 國鼎光電大樓',
+    opentime: ['平日｜'],
     units: '光電系, 鍍膜實驗室, 光電博物館, 光電展示中心, 台達研究中心',
   },
   {
@@ -382,6 +421,7 @@ export default [
     longitude: 121.18991,
     type: '體育場所',
     name: '田徑場',
+    opentime: ['平日｜'],
     units: '操場',
   },
   {
@@ -389,6 +429,7 @@ export default [
     longitude: 121.19262,
     type: '宿舍',
     name: '女一～四舍',
+    opentime: ['平日｜'],
     units: ' ',
   },
   {
@@ -396,6 +437,7 @@ export default [
     longitude: 121.18777,
     type: '系館',
     name: 'E6 工程五館',
+    opentime: ['平日｜'],
     units:
       '資訊工程學系, 資訊電機學院辦公室, 資工系, 資訊工程研究所, 工學院, 工學院精密研究中心, 環境與能源研究中心, 材料科學與工程研究所, 網路學習科技研究所',
   },
@@ -404,6 +446,7 @@ export default [
     longitude: 121.19372,
     type: '系館',
     name: 'I1 管理二館',
+    opentime: ['平日｜'],
     units:
       '管二, 企管, 資管, 財金, 經濟, 企業管理學系, 資訊管理學系, 財務金融學系, 經濟學系, 產驗經濟研究所, 人力資源管理研究所, 工業管理研究所, 會計研究所及管理學術研究中心, 企業資源規劃中心, 管院電算中心',
   },
@@ -412,6 +455,7 @@ export default [
     longitude: 121.19262,
     type: '系館',
     name: 'S1 科學二館',
+    opentime: ['平日｜'],
     units:
       '理學院辦公室, 光電中心辦公室, 地科中心辦公室, 新世代太陽能電池研究中心辦公室, 固態無機化學實驗室, 功能高分子及觸媒實驗室, 奈米觸媒與高能電池實驗室, 化學系系學會, 分子光譜學實驗室, 化學系辦公室, 教授研究室, 大氣系辦公室',
   },
@@ -420,6 +464,7 @@ export default [
     longitude: 121.18709,
     type: '系館',
     name: 'R2 太空及遙測研究中心',
+    opentime: ['平日｜'],
     units: '太遙中心',
   },
   {
@@ -427,6 +472,7 @@ export default [
     longitude: 121.18993,
     type: '體育場所',
     name: '網球場',
+    opentime: ['平日｜09:00-11:45、13:00-20:45', '六日｜09:00-11:45、13:00-20:45', '寒暑假｜09:00-11:45、13:00-20:45'],
     units: ' ',
   },
   {
@@ -434,6 +480,7 @@ export default [
     longitude: 121.18961,
     type: '體育場所',
     name: '室內游泳池',
+    opentime: ['平日｜06:00-11:45、13:00-18:45', '星期六｜06:00-11:45、13:00-16:45', '星期日｜06:00-11:45、13:00-15:45'],
     units: ' ',
   },
   {
@@ -441,6 +488,7 @@ export default [
     longitude: 121.19623,
     type: '宿舍',
     name: '男十三舍',
+    opentime: ['平日｜'],
     units: ' ',
   },
   {
@@ -448,6 +496,7 @@ export default [
     longitude: 121.19268,
     type: '系館',
     name: 'M 鴻經館',
+    opentime: ['平日｜'],
     units: '數學系, 數學系圖書館, 數學系系學會, 數學系系辦公室',
   },
   {
@@ -455,6 +504,7 @@ export default [
     longitude: 121.19079,
     type: '體育場所',
     name: '室外排球場',
+    opentime: ['每天｜07:00-22:00'],
     units: ' ',
   },
   {
@@ -463,124 +513,225 @@ export default [
     longitude: 121.19441,
     type: '校內店家',
     name: '中大iicafe',
+    opentime: ['平日｜'],
     units: ' ',
   },
   {
-    // incorrect position
-    latitude: 24.9676,
-    longitude: 121.19079,
+    latitude: 24.97144,
+    longitude: 121.19231,
     type: '系館',
     name: 'H2 理學院教學館',
+    opentime: ['平日｜'],
     units: ' ',
   },
   {
-    // incorrect position
-    latitude: 24.9676,
-    longitude: 121.19079,
+    latitude: 24.96775,
+    longitude: 121.19298,
     type: '系館',
     name: 'EH 實習一廠',
+    opentime: ['平日｜'],
     units: ' ',
   },
   {
-    // incorrect position
-    latitude: 24.9676,
-    longitude: 121.19079,
+    latitude: 24.96764,
+    longitude: 121.18932,
     type: '系館',
     name: 'H3 實習三廠',
+    opentime: ['平日｜'],
     units: ' ',
   },
+  // {
+  //   // incorrect position
+  //   latitude: 24.9676,
+  //   longitude: 121.19079,
+  //   type: '系館',
+  //   name: 'GE 大氣水文觀測站',
+  //
+  // units: ' ',
+  // },
+  // {
+  //   // incorrect position
+  //   latitude: 24.9676,
+  //   longitude: 121.19079,
+  //   type: '系館',
+  //   name: 'E4 機電實驗室',
+  //
+  // units: ' ',
+  // },
+  // {
+  //   // incorrect position
+  //   latitude: 24.9676,
+  //   longitude: 121.19079,
+  //   type: '系館',
+  //   name: 'EQ 風洞實驗室及品保中心',
+  //
+  // units: ' ',
+  // },
+  // {
+  //   // incorrect position
+  //   latitude: 24.9676,
+  //   longitude: 121.19079,
+  //   type: '系館',
+  //   name: '氣象雷達站',
+  //
+  // units: ' ',
+  // },
+  // {
+  //   // incorrect position
+  //   latitude: 24.9676,
+  //   longitude: 121.19079,
+  //   type: '系館',
+  //   name: '天線區',
+  //
+  // units: ' ',
+  // },
+  // {
+  //   // incorrect position
+  //   latitude: 24.9676,
+  //   longitude: 121.19079,
+  //   type: '系館',
+  //   name: 'G 大氣微量物質分析實驗室',
+  //
+  // units: ' ',
+  // },
   {
-    // incorrect position
-    latitude: 24.9676,
-    longitude: 121.19079,
-    type: '系館',
-    name: 'GE 大氣水文觀測站',
-    units: ' ',
-  },
-  {
-    // incorrect position
-    latitude: 24.9676,
-    longitude: 121.19079,
-    type: '系館',
-    name: 'E4 機電實驗室',
-    units: ' ',
-  },
-  {
-    // incorrect position
-    latitude: 24.9676,
-    longitude: 121.19079,
-    type: '系館',
-    name: 'EQ 風洞實驗室及品保中心',
-    units: ' ',
-  },
-  {
-    // incorrect position
-    latitude: 24.9676,
-    longitude: 121.19079,
-    type: '系館',
-    name: '氣象雷達站',
-    units: ' ',
-  },
-  {
-    // incorrect position
-    latitude: 24.9676,
-    longitude: 121.19079,
-    type: '系館',
-    name: '天線區',
-    units: ' ',
-  },
-  {
-    // incorrect position
-    latitude: 24.9676,
-    longitude: 121.19079,
-    type: '系館',
-    name: 'G 大氣微量物質分析實驗室',
-    units: ' ',
-  },
-  {
-    // incorrect position
-    latitude: 24.9676,
-    longitude: 121.19079,
-    type: '行政處室',
-    name: 'IC 松果館',
-    units: ' ',
-  },
-  {
-    // incorrect position
-    latitude: 24.9676,
-    longitude: 121.19079,
+    latitude: 24.969707521871022,
+    longitude: 121.18982390598758,
     type: '體育場所',
     name: '中大國民運動中心',
+    opentime: ['平日｜'],
     units: ' ',
   },
   {
-    // incorrect position
-    latitude: 24.9676,
-    longitude: 121.19079,
+    latitude: 24.970754151323007,
+    longitude: 121.19148265504151,
     type: '景點',
     name: '中大湖',
+    opentime: ['平日｜'],
     units: ' ',
   },
+  // {
+  //   // incorrect position
+  //   latitude: 24.9676,
+  //   longitude: 121.19079,
+  //   type: '景點',
+  //   name: '學生自主學習空間',
+  //
+  //   units: ' ',
+  // },
   {
-    // incorrect position
-    latitude: 24.9676,
-    longitude: 121.19079,
-    type: '景點',
-    name: '學生自主學習空間',
-    units: ' ',
-  },
-  {
-    // incorrect position
-    latitude: 24.9676,
-    longitude: 121.19079,
+    latitude: 24.966215869682472,
+    longitude: 121.19377032659528,
     type: '景點',
     name: '賈斯汀單車',
+    opentime: ['平日｜'],
     units: ' ',
   },
+  {
+    latitude: 24.968124436298478,
+    longitude: 121.19470733905092,
+    type: 'Ubike',
+    name: 'YouBike 中央大學圖書館',
+    opentime: ['平日｜'],
+    units: ' ',
+  },
+  {
+    latitude: 24.9669702572436,
+    longitude: 121.19103886636081,
+    type: 'Ubike',
+    name: 'YouBike 中央大學依仁堂',
+    opentime: ['平日｜'],
+    units: ' ',
+  },
+  {
+    latitude: 24.968858222143005,
+    longitude: 121.19552705320342,
+    type: '公車站',
+    name: '中央大學警衛室',
+    opentime: ['平日｜'],
+    units: ' ',
+  },
+  {
+    latitude: 24.96978682720721,
+    longitude: 121.19409670274227,
+    type: '公車站',
+    name: '國鼎圖書館',
+    opentime: ['平日｜'],
+    units: ' ',
+  },
+  {
+    latitude: 24.969809638130204,
+    longitude: 121.19148182470599,
+    type: '公車站',
+    name: '中大湖',
+    opentime: ['平日｜'],
+    units: ' ',
+  },
+  {
+    latitude: 24.968368584582855,
+    longitude: 121.19112784558875,
+    type: '公車站',
+    name: '依仁堂',
+    opentime: ['平日｜'],
+    units: ' ',
+  },
+  {
+    latitude: 24.966655334360507,
+    longitude: 121.19169139010519,
+    type: '公車站',
+    name: '中央大學後門',
+    opentime: ['平日｜'],
+    units: ' ',
+  },
+  {
+    latitude: 24.96805803910513,
+    longitude: 121.19558113130034,
+    type: '公車站',
+    name: '中央大學觀景台',
+    opentime: ['平日｜'],
+    units: ' ',
+  },
+  {
+    latitude: 24.966924553195884,
+    longitude: 121.19557841315836,
+    type: '自動繳費機',
+    name: '繳費機1',
+    opentime: ['平日｜'],
+    units: ' ',
+  },
+  {
+    latitude: 24.968688033588645,
+    longitude: 121.19541640391036,
+    type: '自動繳費機',
+    name: '繳費機2',
+    opentime: ['平日｜'],
+    units: ' ',
+  },
+  {
+    latitude: 24.96987595875507,
+    longitude: 121.19316582559476,
+    type: '自動繳費機',
+    name: '繳費機3',
+    opentime: ['平日｜'],
+    units: ' ',
+  },
+  {
+    latitude: 24.966572293699016,
+    longitude: 121.19109098816473,
+    type: '繳費機',
+    name: '繳費機4',
+    opentime: ['平日｜'],
+    units: ' ',
+  },
+  // {
+  //   latitude: 24.968461,
+  //   longitude: 121.195648,
+  //   type: '景點',
+  //   name: '前門警衛室',
+  //   units: ' ',
+  // },
   // 前門警衛室
   // 後門警衛室
-  // ubike
-  // 公車站牌
   // ATM
   // 廁所
   // 繳費機

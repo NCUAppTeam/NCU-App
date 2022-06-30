0692079f47c4b1845fdd6415c4931ab4c7a3d285
Username148576 <jenny20030314@gmail.com>
2022-06-30 23:58:20 +0800

    改buscontroller、加busscreen

---
+++ screens/Map/BusScreen/172-2.js
@@ -0,0 +1,169 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  SafeAreaView,
+  Text,
+  Alert,
+  TouchableOpacity,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  
+  background: {
+    flex: 1,
+    justifyContent: "flex-start",
+  },
+  title: {
+    textAlign: 'center',
+  },
+  fixToText: {
+    flexDirection: 'row',
+  },
+  separator: {
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+});
+const Separator = () => <View style={styles.separator} />;
+function List172({ navigation }) {
+  return (
+  <SafeAreaView style={styles.background}>
+    <View>
+    <Text style={{textAlign:'center', backgroundColor: '#28527A',color:'white', height:50,width:'100%',fontSize:20, textAlignVertical:'center', lineHeight: 50}}>
+        172公車時刻表
+      </Text>
+    </View>
+    
+     <Text style={{ textAlign: 'center',fontSize:20}}>
+       往中央大學
+      </Text>
+    <View>
+      <View style={styles.fixToText}>
+       
+          <Text style={{ textAlign: 'center',width: '50%', backgroundColor: 'white' ,fontSize:20}}>
+           平日
+          </Text>
+          <Text style={{ textAlign: 'center',width: '50%', backgroundColor: 'white' ,fontSize:20}}>
+            假日
+          </Text>
+        
+      </View>
+      <Separator />
+      <View style={styles.fixToText}>
+       
+          <Text style={{ textAlign: 'center',width: '50%', backgroundColor: 'white' ,fontSize:20}}>
+            06:30
+          </Text>
+          <Text style={{ textAlign: 'center',width: '50%', backgroundColor: 'white' ,fontSize:20}}>
+            06:20
+          </Text>   
+      </View>
+      <View style={styles.fixToText}>
+       
+          <Text style={{ textAlign: 'center',width: '50%', backgroundColor: 'white' ,fontSize:20}}>
+            07:00
+          </Text>
+          <Text style={{ textAlign: 'center',width: '50%', backgroundColor: 'white' ,fontSize:20}}>
+            07:00
+          </Text>   
+      </View>
+      <View style={styles.fixToText}>
+       
+          <Text style={{ textAlign: 'center',width: '50%', backgroundColor: 'white' ,fontSize:20}}>
+           08:00
+          </Text>
+          <Text style={{ textAlign: 'center',width: '50%', backgroundColor: 'white' ,fontSize:20}}>
+            07:20
+          </Text>   
+      </View>
+      <View style={styles.fixToText}>
+       
+          <Text style={{ textAlign: 'center',width: '50%', backgroundColor: 'white' ,fontSize:20}}>
+            08:30
+          </Text>
+          <Text style={{ textAlign: 'center',width: '50%', backgroundColor: 'white' ,fontSize:20}}>
+            08:00
+          </Text>   
+      </View>
+      <View style={styles.fixToText}>
+       
+          <Text style={{ textAlign: 'center',width: '50%', backgroundColor: 'white',fontSize:20 }}>
+           09:00
+          </Text>
+          <Text style={{ textAlign: 'center',width: '50%', backgroundColor: 'white' ,fontSize:20}}>
+            08:20
+          </Text>   
+      </View>
+      <View style={styles.fixToText}>
+       
+          <Text style={{ textAlign: 'center',width: '50%', backgroundColor: 'white' ,fontSize:20}}>
+            09:30
+          </Text>
+          <Text style={{ textAlign: 'center',width: '50%', backgroundColor: 'white' ,fontSize:20}}>
+            09:00
+          </Text>   
+      </View>
+      <View style={styles.fixToText}>
+       
+          <Text style={{ textAlign: 'center',width: '50%', backgroundColor: 'white' ,fontSize:20}}>
+            10:00
+          </Text>
+          <Text style={{ textAlign: 'center',width: '50%', backgroundColor: 'white' ,fontSize:20}}>
+            09:20
+          </Text>   
+      </View>
+      <View style={styles.fixToText}>
+       
+          <Text style={{ textAlign: 'center',width: '50%', backgroundColor: 'white' ,fontSize:20}}>
+            10:30
+          </Text>
+          <Text style={{ textAlign: 'center',width: '50%', backgroundColor: 'white' ,fontSize:20}}>
+            10:00
+          </Text>   
+      </View>
+      <View style={styles.fixToText}>
+       
+          <Text style={{ textAlign: 'center',width: '50%', backgroundColor: 'white' ,fontSize:20}}>
+            11:00
+          </Text>
+          <Text style={{ textAlign: 'center',width: '50%', backgroundColor: 'white' ,fontSize:20}}>
+            10:20
+          </Text>   
+      </View>
+      <View style={styles.fixToText}>
+       
+          <Text style={{ textAlign: 'center',width: '50%', backgroundColor: 'white' ,fontSize:20}}>
+            11:30
+          </Text>
+          <Text style={{ textAlign: 'center',width: '50%', backgroundColor: 'white' ,fontSize:20}}>
+            11:00
+          </Text>   
+      </View>
+      <View style={styles.fixToText}>
+       
+          <Text style={{ textAlign: 'center',width: '50%', backgroundColor: 'white' ,fontSize:20}}>
+            12:00
+          </Text>
+          <Text style={{ textAlign: 'center',width: '50%', backgroundColor: 'white',fontSize:20 }}>
+            11:20
+          </Text>   
+      </View>
+      <View style={styles.fixToText}>
+       
+          <Text style={{ textAlign: 'center',width: '50%', backgroundColor: 'white' ,fontSize:20}}>
+            12:25
+          </Text>
+          <Text style={{ textAlign: 'center',width: '50%', backgroundColor: 'white' ,fontSize:20}}>
+            12:00
+          </Text>   
+      </View>
+    </View>
+    
+    <Text style={{ textAlign: 'center', backgroundColor: 'white' ,color:'gray'}}>
+        上次更新:2022/03/10
+      </Text>
+    
+  </SafeAreaView>
+);}
+export default List172;
---
+++ screens/Map/BusScreen/second.js
@@ -0,0 +1,133 @@
+import React, { useState, useEffect} from 'react';
+import {
+  StyleSheet,
+  View,
+  SafeAreaView,
+  ScrollView,
+  Text,
+  Alert,
+  TouchableOpacity,
+  RefreshControl,
+} from 'react-native';
+import {
+  IconButton
+} from 'react-native-paper';
+import BusController from '../../../controller/Bus';
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "flex-start",
+    marginHorizontal: 16,
+  },
+});
+function Second({ navigation }) {
+    const [item, setItems] = useState([]);
+  
+    const [start, setStart] = useState(true);
+  
+    const [refreshing, setRefreshing] = useState(false);
+  
+  
+    const onRefresh = () => {
+        setRefreshing(true);
+        BusController.second().then((res) => {
+          setItems(res);
+          setRefreshing(false);
+        });
+    };
+  
+    useEffect(() => {
+      if(start){
+        setStart(false);
+        onRefresh();
+      }
+      var id;
+      id=setInterval(onRefresh,10000)
+      return () => {
+        clearInterval(id);
+      }
+    },[start]);
+  return (
+    <SafeAreaView style={styles.background} refreshControl={(<RefreshControl  refreshing={refreshing} onRefresh={onRefresh} />)}>
+    <ScrollView>
+    <View style={{flexDirection:'row'}}>
+      <Text style={{textAlign:'center', backgroundColor: '#28527A',color:'white', height:50,width:'100%',fontSize:20, textAlignVertical:'center', lineHeight: 50}}>
+        公車動態
+      </Text>
+      <IconButton
+        onPress={() =>navigation.navigate('地圖')}
+        icon="times"
+        size={20}
+        style={{
+          left:320,
+          top:0,
+          position:'absolute'
+          }}/>
+    </View>
+    
+    <View>
+      <View style={{flexDirection: 'row', height:50}}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('依班次')}
+          style={{ width: '50%', backgroundColor: 'white' }}>
+          <Text style={{textAlign:'center', fontSize:20, textAlignVertical:'center', lineHeight: 50}}>
+            依班次
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() =>navigation.navigate('依站牌')}
+          style={{ width: '50%', backgroundColor: 'white' }}>
+          <Text style={{textAlign:'center', fontSize:20, textAlignVertical:'center', lineHeight: 50}}>
+            依站牌
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+    <Text style={{fontSize:5}}>{'\n'}</Text>
+    {item.map((res)=>(
+      <View>
+        <View>
+        <TouchableOpacity
+          onPress={() => navigation.navigate(res.state)}
+          style={{ width: '100%', backgroundColor: 'white' }}>
+          <View style={{flexDirection:'row'}}>
+            <View style={{width:'45%'}}>
+              <Text style={{textAlign:'center',top:20,fontSize:22,color:'#28527A'}}>
+                {res.state}
+              </Text>
+            </View>
+            <View style={{width:'40%'}}>
+              <Text style={{textAlign:'left',fontSize:20,top:10}}>
+                {res.route1} | {res.time1}
+              </Text>
+              <Text style={{textAlign:'left',fontSize:20,top:20}}>{res.route2} | {res.time2}</Text>
+            </View>
+            <View style={{width:'15%'}}>
+          <IconButton
+            onPress={() => Alert.alert('Left button pressed')}
+            size={20}
+            icon="heart"
+            style={{
+              textAlign:'center'
+              }}/>
+          <IconButton
+            onPress={() => Alert.alert('Left button pressed')}
+            size={20}
+            icon="plus"
+            style={{
+              textAlign:'center'
+              }}/>
+        </View>
+          </View>
+          
+        </TouchableOpacity>
+        </View>
+      <Text style={{fontSize:5}}>{'\n'}</Text>
+    </View>
+    ))}
+    </ScrollView>
+  </SafeAreaView>
+);}
+
+export default Second;

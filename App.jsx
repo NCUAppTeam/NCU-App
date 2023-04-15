f4998b63e24e9f53b4de44f26f2cf5580c7d6b33
rryh.tw <rorarola.tw@gmail.com>
2023-04-15 18:52:46 +0800

    fix(nativebase): searchview catergries is now align enter

--- App.jsx
+++ App.jsx
@@ -2,13 +2,21 @@ import React from 'react';
 import { RootNavigator } from './navigation/RootNavigator';
 import { AuthenticatedUserProvider } from './providers';
 import { BaseTheme } from './theme';
+import { LinearGradient } from 'expo-linear-gradient';
 
 
 import { NativeBaseProvider } from 'native-base';
 
+const config = {
+  dependencies: {
+    'linear-gradient': LinearGradient
+  }
+};
+
+
 export default function App() {
   return (
-    <NativeBaseProvider theme={BaseTheme}>
+    <NativeBaseProvider theme={BaseTheme} config={config}>
       <AuthenticatedUserProvider>
           <RootNavigator />
       </AuthenticatedUserProvider>
--- screens/Event/search.jsx
+++ screens/Event/search.jsx
@@ -9,9 +9,11 @@ import {
 import {
   NativeBaseProvider, Box, HStack, VStack, ZStack, FlatList, Pressable, Icon, Input, Center, Text
 } from 'native-base';
-import { LinearGradient } from 'expo-linear-gradient';
+
 import styles from './style_folder/Styles_search';
 import ActiveController from '../../controller/Active';
+
+
 
 function Search({ navigation }) {
   const genreName = [{ id: 0, name: '揪人共乘' }, { id: 1, name: '校園活動' }, { id: 2, name: '揪人運動' }, { id: 3, name: '系上活動' }, { id: 4, name: '揪人遊戲' }, { id: 5, name: '社團活動' }];
@@ -27,111 +29,109 @@ function Search({ navigation }) {
   }, []);
   return (
     <Box safeArea>
-        <Center>
-          <Input placeholder="搜尋" size="lg" m="2" mt="4" variant="filled" width="90%" height="40px"borderRadius="10" bg="#E5EBF1" py="1" px="2"
-            InputLeftElement={<Icon  ml="2" size="6"  as={AntDesign} name="search1" color="#476685" ></Icon>} 
-            isFocused="true"
-            value={searchQuery}
-            onChangeText={(text) => {
-              setSearchQuery(text);
-              ActiveController.fuseSearchName(text).then((query) => {
-                setData(query);
-              });
-            }}  
-            _focus={{backgroundColor: "#E5EBF1"}}
-            
-            InputRightElement={
-                <Text mr="4" color="#476685" onPress={() => navigation.navigate('list')}>取消</Text>
-            }
-          />
-        </Center>
+      <Center>
+        <Input placeholder="搜尋" size="lg" m="2" mt="4" variant="filled" width="90%" height="40px" borderRadius="10" bg="#E5EBF1" py="1" px="2"
+          InputLeftElement={<Icon ml="2" size="6" as={AntDesign} name="search1" color="#476685" ></Icon>}
+          isFocused="true"
+          value={searchQuery}
+          onChangeText={(text) => {
+            setSearchQuery(text);
+            ActiveController.fuseSearchName(text).then((query) => {
+              setData(query);
+            });
+          }}
+          _focus={{ backgroundColor: "#E5EBF1" }}
 
-        {searchQuery === '' && (
+          InputRightElement={
+            <Text mr="4" color="#476685" onPress={() => navigation.navigate('list')}>取消</Text>
+          }
+        />
+      </Center>
+
+      {searchQuery === '' && (
         <Box style={styles.btnArea}>
           <FlatList
-
             numColumns={2}
             data={genreName}
             keyExtractor={(item) => item.id}
             showsVerticalScrollIndicator={false}
+
+            // columnWrapperStyle={{ justifyContent: 'space-between' }}
+            // contentContainerStyle={{ paddingBottom: 80 }}
+
             renderItem={({ item }) => (
-              <Box flex="1">
-                <LinearGradient
-                  colors={['#D2E6EA26', '#8AC4D026', '#0A6B7E26']}
-                  start={[0.6, 0.0]}
-                  end={[0.5, 0.8]}
-                  style={styles.genrebutton}
+              <Box
+              flex={1} space={2} my={3} mx={2} height="130px"
+                bg={{
+                  linearGradient: {
+                    colors: ['#D2E6EA26', '#0A6B7E26'],
+                    start: [0, 0],
+                    end: [1, 1]
+                  }
+                }}
+                rounded="xl"
+                
+              >
+                <Pressable
+                  onPress={() => {
+                    navigation.navigate('genre', { GenreName: item.name });
+                  }}
                 >
-                  <Pressable
-                    onPress={() => {
-                      navigation.navigate('genre', { GenreName: item.name });
-                    }}
-                  >
-                    <ZStack>
-                      {(item.name === '揪人共乘') && (
-                      <Box>
-                        <FontAwesome name="taxi" size={80} color="#ccc8c8" style={styles.icon} />
-                      </Box>
-                      )}
-                      {(item.name === '揪人運動') && (
-                      <Box>
-                        <MaterialIcons name="sports-volleyball" size={100} color="#ccc8c8" style={styles.icon3} />
-                      </Box>
-                      )}
-                      {(item.name === '揪人遊戲') && (
-                      <Box>
-                        <Ionicons name="ios-game-controller" size={100} color="#ccc8c8" style={styles.icon3} />
-                      </Box>
-                      )}
-                      {(item.name === '校園活動') && (
-                      <Box>
-                        <FontAwesome5 name="school" size={80} color="#ccc8c8" style={styles.icon} />
-                      </Box>
-                      )}
-                      {(item.name === '社團活動') && (
-                      <Box>
-                        <MaterialCommunityIcons name="drama-masks" size={100} color="#ccc8c8" style={styles.icon2} />
-                      </Box>
-                      )}
-                      {(item.name === '系上活動') && (
-                      <Box>
-                        <FontAwesome5 name="trophy" size={80} color="#ccc8c8" style={styles.icon} />
-                      </Box>
-                      )}
-                      <Title style={styles.genreTitle}>
-                        {item.name}
-                      </Title>
-                    </ZStack>
-                  </Pressable>
-                </LinearGradient>
+                  
+                  <ZStack height="100%" alignItems="center" justifyContent="center">
+                    {(item.name === '揪人共乘') && (
+                        <FontAwesome name="taxi" size={80} color="#ccc8c8"/>
+                    )}
+                    {(item.name === '揪人運動') && (
+                        <MaterialIcons name="sports-volleyball" size={100} color="#ccc8c8"/>
+                    )}
+                    {(item.name === '揪人遊戲') && (
+                        <Ionicons name="ios-game-controller" size={100} color="#ccc8c8"/>
+                    )}
+                    {(item.name === '校園活動') && (
+                        <FontAwesome5 name="school" size={80} color="#ccc8c8"/>
+                    )}
+                    {(item.name === '社團活動') && (
+                        <MaterialCommunityIcons name="drama-masks" size={100} color="#ccc8c8"/>
+                    )}
+                    {(item.name === '系上活動') && (
+                        <FontAwesome5 name="trophy" size={80} color="#ccc8c8"/>
+                    )}
+                    <Text textAlign="center">
+                      {item.name}
+                    </Text>
+                
+                  </ZStack>
+                  
+                </Pressable>
+              </Box>
+          )}
+          />
+        </Box>
+      )}
+      {searchQuery !== '' && (
+        <Box style={styles.keywordArea}>
+          <FlatList
+            data={data}
+            keyExtractor={(item) => item.id}
+            showsVerticalScrollIndicator={false}
+            renderItem={({ item }) => (
+              <Box>
+                <Pressable
+                  style={styles.keywordBox}
+                  onPress={() => {
+                    navigation.navigate('details', { Cd: item.item.id, prepage: 'search' });
+                  }}
+                >
+                  <Text style={styles.keywordBoxText}>{item.item.name}</Text>
+                </Pressable>
               </Box>
             )}
           />
         </Box>
-        )}
-        {searchQuery !== '' && (
-          <Box style={styles.keywordArea}>
-            <FlatList
-              data={data}
-              keyExtractor={(item) => item.id}
-              showsVerticalScrollIndicator={false}
-              renderItem={({ item }) => (
-                <Box>
-                  <Pressable
-                    style={styles.keywordBox}
-                    onPress={() => {
-                      navigation.navigate('details', { Cd: item.item.id, prepage: 'search' });
-                    }}
-                  >
-                    <Text style={styles.keywordBoxText}>{item.item.name}</Text>
-                  </Pressable>
-                </Box>
-              )}
-            />
-          </Box>
-        )}
+      )}
 
-      
+
     </Box>
   );
 }

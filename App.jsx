bea6f7bd83dee494c21b403fd829ff3149cc9e89
1989ONCE <susanloveselena@gmail.com>
2023-04-20 18:11:08 +0800

    fix: searchHeader

--- App.jsx
+++ App.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
 
 import { NativeBaseProvider } from 'native-base';
+import { LinearGradient } from 'expo-linear-gradient';
 import { RootNavigator } from './navigation/RootNavigator';
 import { AuthenticatedUserProvider } from './providers';
-import { BaseTheme } from './theme';
-import { LinearGradient } from 'expo-linear-gradient';
-
-
-import { NativeBaseProvider } from 'native-base';
+import { BaseTheme } from './theme/index';
 
 const config = {
   dependencies: {
-    'linear-gradient': LinearGradient
-  }
+    'linear-gradient': LinearGradient,
+  },
 };
-
 
 export default function App() {
   return (

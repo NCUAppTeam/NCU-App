f75eeb287584e67efc46582f5c20fbd7ebcaf4c8
1989ONCE <susanloveselena@gmail.com>
2023-05-11 22:15:30 +0800

    fix: Dialog Bug

--- screens/Event/manage.jsx
+++ screens/Event/manage.jsx
@@ -1,14 +1,14 @@
 /* eslint-disable max-len */
 import React, { useState, useEffect } from 'react';
 import {
-  Text, View, SafeAreaView, TextInput, RefreshControl,
+  Text, SafeAreaView, TextInput, RefreshControl,
   ScrollView, TouchableOpacity, Image, TouchableHighlight,
 } from 'react-native';
 
 import Dialog, { DialogContent } from 'react-native-popup-dialog';
 import { AntDesign, Feather } from '@expo/vector-icons';
 import {
-  Box, Divider, Heading, VStack, HStack, FlatList,
+  Box, Divider, Heading, VStack, HStack, FlatList, NativeBaseProvider,
 } from 'native-base';
 import { LinearGradient } from 'expo-linear-gradient';
 import ActiveController from '../../controller/Active';
@@ -98,84 +98,55 @@ function Manage({ route, navigation }) {
             width={360}
             height={285}
             visible={showDialog1}
+            onTouchOutside={() => {
+              setShowDialog1(false);
+            }}
           >
-            <DialogContent style={{
-              paddingBottom: 0,
-              borderBottomWidth: 1,
-              borderBottomColor: '#e5e5e5',
-            }}
-            >
-              <View style={{
-                flexDirection: 'row',
-                paddingBottom: 5,
-                justifyContent: 'space-between',
-              }}
-              >
-                <View>
-                  <Text style={{
-                    textAlign: 'left',
-                    color: '#1f2937',
-                    fontSize: 16,
-                    fontWeight: '400',
-                    marginTop: 17,
-                    marginBottom: 10,
-                  }}
-                  >
-                    刪除活動&ensp;
+            <NativeBaseProvider>
+              <DialogContent>
+                <Box style={styles.DeleteTitle}>
+                  <Box>
+                    <Text style={styles.DeleteTitleText}>
+                      刪除活動&ensp;
+                    </Text>
+                  </Box>
+                  <Box>
+                    <Feather
+                      name="x"
+                      size={26}
+                      color="#1F2937"
+                      style={{
+                        marginTop: 15,
+                      }}
+                      onPress={() => { setShowDialog1(false); }}
+                    />
+                  </Box>
+                </Box>
+                <Divider my={2} bg="#bfbebe" w="100%" />
+                <Box style={styles.removeBox}>
+                  <Text style={styles.textSetting1}>
+                    注意事項：
                   </Text>
-                </View>
-                <View>
-                  <Feather
-                    name="x"
-                    size={26}
-                    color="#1F2937"
-                    style={{
-                      marginTop: 15,
-                    }}
-                    onPress={() => { setShowDialog1(false); }}
-                  />
-                </View>
-              </View>
-            </DialogContent>
-
-            <DialogContent style={{ paddingTop: 10, paddingBottom: 20 }}>
-              <View style={styles.removeBox}>
-                <Text style={{ fontSize: 14 }}>
-                  注意事項：
-                </Text>
-                <Text style={{ fontSize: 14, marginBottom: 5 }}>
-                  1. 這將會刪除活動的資料，並同時移除所有此活動
-                  {'\n'}
+                  <Text style={styles.textSetting2}>
+                    1. 這將會刪除活動的資料，並同時移除所有此活動
+                    {'\n'}
                             &emsp;的參加者。
-                </Text>
-                <Text style={{
-                  fontSize: 14, marginBottom: 5,
-                }}
-                >
-                  2. 當此活動被刪除，系統將自動發送通知給此活動
-                  {'\n'}
+                  </Text>
+                  <Text style={styles.textSetting2}>
+                    2. 當此活動被刪除，系統將自動發送通知給此活動
+                    {'\n'}
                             &emsp;的所有參加者，讓他們知道活動已被刪除。
-                </Text>
-                <Text style={{
-                  fontSize: 14, marginBottom: 5, color: '#ef4444',
-                }}
-                >
-                  3. 一旦按下下方紅色刪除按鈕，即立刻執行刪除，
-                  {'\n'}
+                  </Text>
+                  <Text style={[styles.textSetting2, { color: '#ef4444' }]}>
+                    3. 一旦按下下方紅色刪除按鈕，即立刻執行刪除，
+                    {'\n'}
                             &emsp;且無法復原！
-                </Text>
-              </View>
-            </DialogContent>
-            <DialogContent style={{
-              height: 81,
-              width: 360,
-              backgroundColor: '#f3f4f6',
-              justifyContent: 'center',
-              alignItems: 'flex-end',
-            }}
-            >
-              <View style={{ flexDirection: 'row' }}>
-                <View
+                  </Text>
+                </Box>
+
+              </DialogContent>
+              <Box style={styles.DeleteDialogBottom}>
+                <Box
                   style={{ marginTop: 10, marginRight: 10 }}
                 >
                   <Text
@@ -189,9 +160,11 @@ function Manage({ route, navigation }) {
                     取消
 
                   </Text>
-                </View>
-                <View
-                  style={{ marginTop: 10, backgroundColor: '#ef4444', borderRadius: 4 }}
+                </Box>
+                <Box
+                  style={{
+                    marginTop: 10, backgroundColor: '#ef4444', borderRadius: 4, marginRight: 10,
+                  }}
                 >
                   <Text
                     style={{
@@ -206,9 +179,9 @@ function Manage({ route, navigation }) {
                     刪除
 
                   </Text>
-                </View>
-              </View>
-            </DialogContent>
+                </Box>
+              </Box>
+            </NativeBaseProvider>
           </Dialog>
         </Box>
         <Box style={styles.headerEditView}>
@@ -229,7 +202,7 @@ function Manage({ route, navigation }) {
           <Box key={id} style={{ marginTop: 20, marginHorizontal: 8 }}>
             <Heading>{name}</Heading>
             <Divider my={2} bg="#bfbebe" /* my=margin-top and margin-bottom */ />
-            <View style={{ flexDirection: 'column' }}>
+            <Box style={{ flexDirection: 'column' }}>
               <Text style={{ fontSize: 18, color: 'black', marginBottom: 10 }}>發送通知</Text>
               <TextInput
                 style={styles.messageBox}
@@ -258,7 +231,7 @@ function Manage({ route, navigation }) {
                   <Text style={styles.manageSendMessagebtnText}>發送給所有參與者</Text>
                 </TouchableOpacity>
               </LinearGradient>
-            </View>
+            </Box>
             <Divider marginTop={3} bg="#bfbebe" /* my=margin-top and margin-bottom */ />
             <Box>
               <HStack style={{ justifyContent: 'space-between' }}>
@@ -372,27 +345,15 @@ function Manage({ route, navigation }) {
                         width={358}
                         height={240}
                         visible={showDialog2}
-                        dialogTitle={(
-                          <Box>
-                            <HStack style={{
-                              flexDirection: 'row',
-                              paddingBottom: 5,
-                              justifyContent: 'space-between',
-                              alignItems: 'center',
-                              marginLeft: 10,
-                            }}
-                            >
+                        onTouchOutside={() => {
+                          setShowDialog2(false);
+                        }}
+                      >
+                        <NativeBaseProvider>
+                          <DialogContent>
+                            <Box style={styles.RemoveDialog}>
                               <Box>
-                                <Text style={{
-                                  textAlign: 'left',
-                                  color: '#1f2937',
-                                  fontSize: 16,
-                                  fontWeight: '400',
-                                  marginTop: 17,
-                                  marginBottom: 10,
-                                  alignItems: 'center',
-                                }}
-                                >
+                                <Text style={styles.RemoveDialogTitle}>
                                   移除&ensp;
                                   {deletePerson.name}
                                 </Text>
@@ -402,110 +363,96 @@ function Manage({ route, navigation }) {
                                 size={26}
                                 color="#1F2937"
                                 style={{
-                                  marginTop: 10,
-                                  marginRight: 10,
+                                  marginTop: 15,
                                 }}
                                 onPress={() => { setShowDialog2(false); }}
                               />
-                            </HStack>
+                            </Box>
                             <Divider style={{ marginTop: 5 }} bg="#e5e5e5" />
-                            <Box style={styles.removeBox2}>
-                              <Box style={{
-                                paddingTop: 10, paddingBottom: 10, marginLeft: 10,
-                              }}
-                              >
-                                <Text style={{ fontSize: 14, marginBottom: 5 }}>
-                                  注意事項：
-                                </Text>
-                                <Text style={{ fontSize: 14, marginBottom: 5 }}>
-                                  1. 這將會把
-                                  {' '}
-                                  {deletePerson.name}
-                                  {' '}
-                                  從此活動移除。
-                                </Text>
-                                <Text style={{
-                                  fontSize: 14, marginBottom: 5, color: '#ef4444', width: 350,
+                            <Box>
+                              <Box style={styles.removeBox2}>
+                                <Box style={{
+                                  paddingTop: 10, paddingBottom: 10,
                                 }}
                                 >
-                                  2. 當
-                                  {' '}
-                                  {deletePerson.name}
-                                  {' '}
-                                  被移除，系統將自動發送通知給
-                                  {' '}
-                                  {deletePerson.name}
-                                  {' '}
-                                  ，
-                                  {'\n'}
-                                  {'    '}
-                                  {deletePerson.name}
-                                  {' '}
-                                  將知道自己被從活動移除。
-                                </Text>
-                              </Box>
-                              <Box style={{
-                                flex: 1,
-                                width: 357,
-                                backgroundColor: '#f3f4f6',
-                                justifyContent: 'center',
-                                alignItems: 'flex-end',
-                                borderRadius: 5,
-                              }}
-                              >
-                                <HStack>
-                                  <Box
-                                    style={{ marginTop: 5, marginRight: 10 }}
-                                  >
-                                    <Text
-                                      style={{
-                                        fontSize: 14, color: '#64748B', padding: 10,
-                                      }}
-                                      onPress={() => {
-                                        setShowDialog2(false);
-                                      }}
-                                    >
-                                      取消
-                                    </Text>
-                                  </Box>
-                                  <Box
-                                    style={{
-                                      marginTop: 5, marginRight: 15, backgroundColor: '#ef4444', borderRadius: 4,
-                                    }}
-                                  >
-                                    <Text
-                                      style={{
-                                        color: '#ffffff', padding: 10,
-                                      }}
-                                      onPress={() => {
-                                        setShowDialog2(false);
-                                        ActiveController.removeAttendee(passedID, deletePerson.studentID);
-                                        ActiveController.getOneActive(passedID).then((event) => {
-                                          MessageController.addMessage({
-                                            message: `以下為自動寄出的訊息：你已被移出【${event[0].name}】的參加名單, 有任何問題請與活動負責人聯繫`,
-                                            send: user,
-                                            receive: deletePerson.studentID,
-                                            sendTime: new Date(),
-                                            readForSender: true,
-                                            readForReceiver: false,
-                                            image: '',
-                                          }, user);
-                                        });
-                                      }}
-                                    >
-                                      移除
-
-                                    </Text>
-                                  </Box>
-                                </HStack>
+                                  <Text style={styles.textSetting2}>
+                                    注意事項：
+                                  </Text>
+                                  <Text style={styles.textSetting2}>
+                                    1. 這將會把
+                                    {' '}
+                                    {deletePerson.name}
+                                    {' '}
+                                    從此活動移除。
+                                  </Text>
+                                  <Text style={[styles.textSetting2, { color: '#ef4444', width: 350 }]}>
+                                    2. 當
+                                    {' '}
+                                    {deletePerson.name}
+                                    {' '}
+                                    被移除，系統將自動發送通知給
+                                    {' '}
+                                    {deletePerson.name}
+                                    {' '}
+                                    ，
+                                    {'\n'}
+                                    {'    '}
+                                    {deletePerson.name}
+                                    {' '}
+                                    將知道自己被從活動移除。
+                                  </Text>
+                                </Box>
                               </Box>
                             </Box>
+
+                          </DialogContent>
+                          <Box style={styles.removeDialogBottom}>
+                            <Box
+                              style={{ marginTop: 10, marginRight: 10 }}
+                            >
+                              <Text
+                                style={{
+                                  fontSize: 14, color: '#64748B', padding: 10,
+                                }}
+                                onPress={() => {
+                                  setShowDialog2(false);
+                                }}
+                              >
+                                取消
+                              </Text>
+                            </Box>
+                            <Box
+                              style={{
+                                marginTop: 10, marginRight: 15, backgroundColor: '#ef4444', borderRadius: 4,
+                              }}
+                            >
+                              <Text
+                                style={{
+                                  color: '#ffffff', padding: 10,
+                                }}
+                                onPress={() => {
+                                  setShowDialog2(false);
+                                  ActiveController.removeAttendee(passedID, deletePerson.studentID);
+                                  ActiveController.getOneActive(passedID).then((event) => {
+                                    MessageController.addMessage({
+                                      message: `以下為自動寄出的訊息：你已被移出【${event[0].name}】的參加名單, 有任何問題請與活動負責人聯繫`,
+                                      send: user,
+                                      receive: deletePerson.studentID,
+                                      sendTime: new Date(),
+                                      readForSender: true,
+                                      readForReceiver: false,
+                                      image: '',
+                                    }, user);
+                                  });
+                                }}
+                              >
+                                移除
+
+                              </Text>
+                            </Box>
                           </Box>
-                          )}
-                        onTouchOutside={() => {
-                          setShowDialog2(false);
-                        }}
-                      />
+                        </NativeBaseProvider>
+                      </Dialog>
                     </Box>
                     <Box style={styles.MessagebtnInManage}>
                       <TouchableHighlight>

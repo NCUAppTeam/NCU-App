b003dc9c041f204495124bc4d13379f75c63bbac
1989ONCE <susanloveselena@gmail.com>
2023-06-01 02:15:09 +0800

    feature: Settings Page

---
+++ screens/Home/components/SettingHeader.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {
+  AntDesign
+} from '@expo/vector-icons'
+import {
+  Box, Center, Text
+} from 'native-base'
+
+export function SettingHeader ({ navigation }) {
+  return (
+    <Box flexDirection="row" safeArea>
+        <Center>
+            <Box flex={1.5} marginLeft={6} onPress={() => { navigation.navigate('homepage') }}>
+                <AntDesign
+                name="arrowleft"
+                size={28}
+                color="#476685"
+                />
+            </Box>
+        </Center>
+        <Box flex={2.2} />
+        <Box flex={4}>
+            <Text fontSize="2xl" color="#28527A" justifyContent="center" marginTop={-1}>設定</Text>
+        </Box>
+    </Box>
+
+  )
+}
+export default { SettingHeader }

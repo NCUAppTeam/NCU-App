7b0da2240d06b1e2334bba16802814e0f8910ab2
1989ONCE <susanloveselena@gmail.com>
2023-09-23 03:59:35 +0800

    home screen UI update, create tutorial card

--- screens/Home/homePage.jsx
+++ screens/Home/homePage.jsx
@@ -1,20 +1,48 @@
 import React, { useState, useEffect } from 'react'
-import { Image, RefreshControl } from 'react-native'
+import { RefreshControl } from 'react-native'
 import { Box, ScrollView } from 'native-base'
 import { HomeHeader } from './components/forHome/HomeHeader'
 import { Calendar } from './components/forHome/Calender'
 import { EventCard } from './components/forHome/EventCard'
 import { Tutorial } from './components/forHome/Tutorial'
 import ActiveController from '../../controller/Active'
+import homeController from '../../controller/Home'
 
 import styles from './styles_folder/Styles'
 
 function HomePage ({ navigation }) {
+  const [showNow, setShowNow] = useState([])
   const [refreshing, setRefreshing] = useState(false)
+  const [card, setCard] = useState()
+
   const onRefresh = async () => {
     setRefreshing(true)
+    ActiveController.getParticipatedActive().then((res) => {
+      setShowNow(res)
+    }).catch((err) => {
+      throw err
+    })
+    homeController.getTutorial().then((res) => {
+      setCard(res)
+      console.log(res)
+    }).catch((err) => {
+      throw err
+    })
     setRefreshing(false)
   }
+
+  useEffect(() => {
+    ActiveController.getParticipatedActive().then((res) => {
+      setShowNow(res)
+    }).catch((err) => {
+      throw err
+    })
+    homeController.getTutorial().then((res) => {
+      setCard(res)
+    }).catch((err) => {
+      throw err
+    })
+  }, [])
   return (
       <Box safeArea style={styles.container}>
         <HomeHeader navigation={navigation} />
@@ -28,9 +56,11 @@ function HomePage ({ navigation }) {
         )}
         >
           <Calendar navigation={navigation} />
-          <EventCard navigation={navigation} />
-          <Tutorial navigation={navigation} />
+          <EventCard showNow={showNow} />
+          <Tutorial card={card}/>
+
         </ScrollView>
+
       </Box>
 
   )

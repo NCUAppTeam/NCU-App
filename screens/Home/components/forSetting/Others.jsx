908671c90464a9fdc90bc6895cd34266ff486385
1989ONCE <susanloveselena@gmail.com>
2024-04-06 23:12:05 +0800

    feat: add User deletion feature

--- screens/Home/components/forSetting/Others.jsx
+++ screens/Home/components/forSetting/Others.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import {
   Box, Center, Text, Pressable, Link
 } from 'native-base'
-import { getAuth, signOut } from 'firebase/auth'
+import { deleteUserInfo } from '../../../../controller/Setting'
+import { getAuth, signOut, deleteUser } from 'firebase/auth'
 
 export function Others ({ navigation }) {
   const auth = getAuth()
@@ -25,9 +26,37 @@ export function Others ({ navigation }) {
               marginTop={'2'} backgroundColor={'#FFFFFF'} borderWidth={'5'} borderColor={'#ffffff'} href="https://ncuappteam.github.io/">
               關於NCU app
             </Link>
-
+            {!auth.currentUser.isAnonymous && <Pressable w={'90%'} marginTop={'2'} backgroundColor={'#FFFFFF'} borderWidth={'5'} borderColor={'#ffffff'}
+            onPress={() => {
+              deleteUserInfo(auth.currentUser.uid)
+              deleteUser(auth.currentUser).then(() => {
+                // User deleted.
+                console.log('User deleted.')
+              
+            })
+              signOut(auth).then(() => {
+                // Sign-out successful.
+                console.log('Sign-out successful.')
+              }).catch((error) => {
+                throw error
+              })
+              }}
+          >
+            <Text bold color={'#EF4444'} fontSize={'xl'}>
+              刪除帳號
+            </Text>
+          </Pressable>
+          }
             <Pressable w={'90%'} marginTop={'2'} backgroundColor={'#FFFFFF'} borderWidth={'5'} borderColor={'#ffffff'}
             onPress={() => {
+              if(auth.currentUser.isAnonymous){
+                deleteUser(auth.currentUser).then(() => {
+                  // User deleted.
+                  console.log('User deleted.')
+                }).catch((error) => {
+                  throw error
+                })
+              } 
               signOut(auth).then(() => {
                 // Sign-out successful.
                 console.log('Sign-out successful.')

16e0f203ca291e5645f60ae27f53a64698f19886
David Day <dj6082013@gmail.com>
2024-09-11 14:57:36 -0700

    Add button component.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,14 @@
 import { ReactNode } from "react"
 
 type Props = {
-  children: ReactNode
+  children: ReactNode,
+  type: 'submit' | 'reset' | 'button',
 }
 
-export function Button({ children }: Props) {
+export function Button(props: Props) {
   return (
-    <div className="flex">
-      {children}
-    </div>
+    <button className="bg-cyan-600 text-white p-2 rounded" type={props.type} >
+      {props.children}
+    </button>
   )
 }

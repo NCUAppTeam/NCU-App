5ecb3d190c59e3ba2b27067ce2bada7263f29d83
1989ONCE <susanloveselena@gmail.com>
2025-04-29 15:23:25 +0800

    refactor and fix type check failure

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,6 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router';
 import { useState } from 'react';
 import logo from '../assets/logo.png';
+import IsRememberSection from '../components/pages/user/isRememberSection';
 import { supabase } from '../utils/supabase';
 
 export const Route = createFileRoute('/login')({
@@ -70,7 +71,7 @@ function LoginPage() {
             onChange={(e) => setPassword(e.target.value)}
           />
 
-          {/* <IsRememberSection isRemember={isRemember} setIsRemember={setIsRemember} /> */}
+          <IsRememberSection isRemember={isRemember} setIsRemember={setIsRemember} text={Labels.remember} />
 
           {
             isError
@@ -114,28 +115,4 @@ class Labels {
   static readonly login = '登入';
   static readonly signUp = 'NCU Portal 快速註冊';
   static readonly wrongAccountOrPassword = '帳號或密碼錯誤，請確認後再登入';
-}
-
-function IsRememberSection({ isRemember, setIsRemember }: { isRemember: boolean, setIsRemember: React.Dispatch<React.SetStateAction<boolean>> }) {
-  return (
-    <div className='flex flex-row items-center gap-4'>
-      {/* The style of the checkbox is refered from https://www.material-tailwind.com/docs/html/checkbox */}
-      <label htmlFor='remember' className='flex items-center cursor-pointer relative'>
-        <input
-          type='checkbox'
-          className='peer size-5 cursor-pointer transition-all appearance-none rounded bg-white border-2 border-gray-600 checked:bg-cyan-500 checked:border-cyan-500' id='remember'
-          checked={isRemember}
-          onChange={(e) => setIsRemember(e.target.checked)}
-        />
-        <span className='absolute text-white opacity-0 peer-checked:opacity-100 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
-          <svg xmlns='http://www.w3.org/2000/svg' className='h-3.5 w-3.5' viewBox='0 0 20 20' fill='currentColor' stroke='currentColor' strokeWidth='1' aria-hidden="true">
-            <path fillRule='evenodd' d='M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z' clipRule='evenodd'></path>
-          </svg>
-        </span>
-      </label>
-      <label htmlFor='remember' className='text-white font-bold text-2xl'>
-        {Labels.remember}
-      </label>
-    </div>
-  );
 }

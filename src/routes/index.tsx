cbd39bbd84397284c2644a1938a140d0e0a00008
nelsonkct <nelsonkuo0430@gmail.com>
2025-05-20 23:37:51 +0800

    feat: show created events on home page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,31 @@
 import { createFileRoute } from '@tanstack/react-router';
+import EventController from '../backend/event/Controllers/EventController';
+import { DBEvent } from '../backend/event/Entities/Event';
+import EventCard from '../components/pages/events/eventCard';
 import { SchoolCalendar } from '../components/pages/home/schoolCalendar';
+import { UserController } from '../controllers/user';
 import { AuthGuard } from '../utils/auth';
 
 export const Route = createFileRoute('/')({
     beforeLoad: AuthGuard,
+    loader: async () => {
+        const uuid = (await UserController.get()).id
+        const eventController = new EventController();
+        const events = await eventController.getUserCreatedEvents(uuid);
+        return { events };
+    },
     component: HomeIndex
 })
 
 function HomeIndex() {
+    const { events } = Route.useLoaderData() as { events: DBEvent[] };
+    
     return (
         <>
             <div className='grid justify-items-center w-full space-y-4'>
+                {events.map((event: DBEvent) => (
+                    <EventCard key={event.id} event={event} />
+                ))}
                 <SchoolCalendar />
             </div>
         </>
